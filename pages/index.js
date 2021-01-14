0f4af0b53ef0d8fcbb1529d4686900106db24879
Tim <tw5033@hotmail.com>
2021-01-13 17:58:14 -0800

    add commit logo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import styles from 'styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img src='/images/commit-logo-white.svg' alt='commit-logo'/>
+      </div>
       Hello World!
     </div>
   )
