ef30272106a91cb73b57362ac7b03ce6644c1c6f
Tim <tw5033@hotmail.com>
2021-01-22 15:44:42 -0800

    f(ci): update to deploy onto s3
    clean extra ci commands

--- docs/_next/static/mxcggHy1wyIf4_LOFrGt3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F5a4fc936f29d8fa26a2b.css","static\u002Fchunks\u002Fpages\u002Findex-3c2f34087e11a205ee45.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3cfbae13b59a0f4e814b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
