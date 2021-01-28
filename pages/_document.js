144635177ad515b8ee9d7ba92f54ec4d8167c424
Tim <tw5033@hotmail.com>
2021-01-27 16:24:51 -0800

    f(ga): add google analytics script to head

---
+++ pages/_document.js
@@ -0,0 +1,47 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+const GA_TRACKING_ID = 'GTM-WSP5CD7'
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              title="Google Tag Manager"
+              src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
